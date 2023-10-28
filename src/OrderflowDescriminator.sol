099a5d06d653c8668c2e3085ca8c07a3269c85db
KRD <44735306+KRD-Kai@users.noreply.github.com>
2023-10-29 00:33:25 +0100

    Add price before and and after calc in beforeSwap

--- src/OrderflowDescriminator.sol
+++ src/OrderflowDescriminator.sol
@@ -3,28 +3,33 @@ pragma solidity ^0.8.20;
 
 import {BaseHook} from "periphery-next/BaseHook.sol";
 
+import {FullMath} from "@uniswap/v4-core/contracts/libraries/FullMath.sol";
+
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {Hooks} from "v4-core/libraries/Hooks.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
 import {PoolKey} from "v4-core/types/PoolKey.sol";
 import {FeeLibrary} from "v4-core/libraries/FeeLibrary.sol";
-
 import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
+import {SqrtPriceMath} from "@uniswap/v4-core/contracts/libraries/SqrtPriceMath.sol";
+
+import "forge-std/console.sol";
 
 contract OrderflowDescriminator is BaseHook {
     using PoolIdLibrary for PoolKey;
     using FeeLibrary for uint24;
 
+    IPoolManager internal _poolManager;
     error MustUseDynamicFee();
 
-    uint24 internal fee;
+    uint24 internal _fee;
 
     // Cross-pool user state
     mapping(address user => uint256 swapCount) public globalUserSwapCount;
 
     /// @dev public for testing
-    function setFee(uint24 _fee) public {
-        fee = _fee;
+    function setFee(uint24 fee_) public {
+        _fee = fee_;
     }
 
     function getFee(
@@ -33,10 +38,12 @@ contract OrderflowDescriminator is BaseHook {
         IPoolManager.SwapParams calldata,
         bytes calldata
     ) public view returns (uint24) {
-        return fee;
+        return _fee;
     }
 
-    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
+    constructor(IPoolManager poolManager_) BaseHook(poolManager) {
+        _poolManager = poolManager_;
+    }
 
     function getHooksCalls() public pure override returns (Hooks.Calls memory) {
         return
@@ -58,20 +65,42 @@ contract OrderflowDescriminator is BaseHook {
 
     function beforeInitialize(
         address,
-        PoolKey calldata key,
+        PoolKey calldata key_,
         uint160,
         bytes calldata
     ) external pure override returns (bytes4 selector) {
-        if (!key.fee.isDynamicFee()) revert MustUseDynamicFee();
+        if (!key_.fee.isDynamicFee()) revert MustUseDynamicFee();
         return BaseHook.beforeInitialize.selector;
     }
 
     function beforeSwap(
         address,
-        PoolKey calldata,
-        IPoolManager.SwapParams calldata,
+        PoolKey calldata poolKey_,
+        IPoolManager.SwapParams calldata swapParams_,
         bytes calldata
     ) external override returns (bytes4) {
+        PoolId poolId = PoolIdLibrary.toId(poolKey_);
+        uint128 liquidity = _poolManager.getLiquidity(poolId);
+
+        (uint160 sqrtPriceX96, , , , , ) = _poolManager.getSlot0(
+            PoolIdLibrary.toId(poolKey_)
+        );
+
+        uint160 nextSqrtPriceX96 = SqrtPriceMath
+            .getNextSqrtPriceFromAmount0RoundingUp(
+                sqrtPriceX96,
+                liquidity,
+                uint256(swapParams_.amountSpecified),
+                swapParams_.zeroForOne
+            );
+
+        uint256 priceBeforeSwap = _getPrice(sqrtPriceX96);
+        uint256 priceAfterSwap = _getPrice(nextSqrtPriceX96);
+
+        console.log(priceBeforeSwap);
+        console.log(priceAfterSwap);
+        console.log();
+
         return BaseHook.beforeSwap.selector;
     }
 
@@ -84,5 +113,11 @@ contract OrderflowDescriminator is BaseHook {
     ) external override returns (bytes4) {
         globalUserSwapCount[tx.origin] += 1;
         return BaseHook.afterSwap.selector;
+    }
+
+    function _getPrice(
+        uint160 sqrtPriceX96_
+    ) internal pure returns (uint256 price) {
+        price = FullMath.mulDiv(sqrtPriceX96_, sqrtPriceX96_, 2 ** 96);
     }
 }
